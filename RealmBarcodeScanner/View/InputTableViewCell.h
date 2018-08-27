e2028c1503aa409e304e0bde05d368010ea2bf01
Shaun Anderson <shaun.anderson@outlook.com>
2018-08-27 16:28:48 +1000

    Added InputTableViewCell

---
+++ RealmBarcodeScanner/View/InputTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  InputTableViewCell.h
+//  RealmBarcodeScanner
+//
+//  Created by Shaun Anderson on 27/8/18.
+//  Copyright © 2018 Shaun Anderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputTableViewCell : UITableViewCell
+
+@end
---
+++ RealmBarcodeScanner/View/InputTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  InputTableViewCell.m
+//  RealmBarcodeScanner
+//
+//  Created by Shaun Anderson on 27/8/18.
+//  Copyright © 2018 Shaun Anderson. All rights reserved.
+//
+
+#import "InputTableViewCell.h"
+
+@implementation InputTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
