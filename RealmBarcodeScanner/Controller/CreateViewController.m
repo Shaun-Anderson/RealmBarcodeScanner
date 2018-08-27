2b719dcc5f5b678c5b63a917f3bc3dc457626c45
Shaun Anderson <shaun.anderson@outlook.com>
2018-08-27 16:31:39 +1000

    Set up tableview delegate and datasource methods and added an addToRealm method.

--- RealmBarcodeScanner/Controller/CreateViewController.m
+++ RealmBarcodeScanner/Controller/CreateViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "CreateViewController.h"
+#import "InputTableViewCell.h"
+#import "DatabaseObject.h"
 
 @interface CreateViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,15 +20,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)saveButtonPressed:(id)sender {
+    
+}
 
 - (IBAction)cancelButtonPressed {
     [self dismissViewControllerAnimated:true completion:nil];
+}
+
+- (void)addItemToRealm:(NSString*)itemName MetaData:(NSString*)itemMetaData{
+    DatabaseObject *newObject = [[DatabaseObject alloc] init];
+    newObject.name = itemName;
+    newObject.metaDataString = itemMetaData;
+    
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    [realm transactionWithBlock:^{
+        [realm addObject:newObject];
+    }];
+    
+}
+
+// MARK: - TableView Delegate/DataSource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"inputCell";
+    
+    InputTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[InputTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    //cell.textLabel.text = [objects objectAtIndex:indexPath.row].name;
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 2;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return @"INPUT";
 }
 
 @end
