e524865c52aab67d75482a75239813fb89611d1c
Shaun Anderson <shaun.anderson@outlook.com>
2018-08-31 16:43:06 +1000

    TableView now loads data from form.

--- RealmBarcodeScanner/Controller/CreateViewController.m
+++ RealmBarcodeScanner/Controller/CreateViewController.m
@@ -8,7 +8,9 @@
 
 #import "CreateViewController.h"
 #import "InputTableViewCell.h"
+#import "FormStepperTableViewCell.h"
 #import "DatabaseObject.h"
+#import "Form.h"
 
 @interface CreateViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -17,13 +19,24 @@
 
 @implementation CreateViewController
 
+DatabaseObject *newObject;
+Form* form;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     // Do any additional setup after loading the view.
     _tableView.delegate = self;
     _tableView.dataSource = self;
-
+    
+    form = [[Form alloc] init];
+    [form addSection:@"Input"];
+    [form addTextField:@"Name"];
+    [form addTextField:@"Description"];
+    [form addSection:@"Next"];
+    [form addStepper:@"Amount"];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -55,27 +68,52 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *simpleTableIdentifier = @"inputCell";
+    NSString *stepperCellIdentifier = @"formStepperCell";
     
-    InputTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
-    
-    if (cell == nil) {
-        cell = [[InputTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    switch (form.sections[indexPath.section].inputs[indexPath.row].typeFlag) {
+        case Text:
+        {
+            InputTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+            
+            if (cell == nil) {
+                cell = [[InputTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+            }
+            
+            cell.titleLabel.text = form.sections[indexPath.section].inputs[indexPath.row].inputName;
+            return cell;
+        }
+            
+        case Stepper:
+        {
+            FormStepperTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:stepperCellIdentifier];
+            
+            if (cell == nil) {
+                cell = [[FormStepperTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:stepperCellIdentifier];
+            }
+            
+            cell.titleLabel.text = form.sections[indexPath.section].inputs[indexPath.row].inputName;
+            return cell;
+        }
+            
+        default:
+        {
+            UITableViewCell *cell = [[UITableViewCell alloc] init];
+            return cell;
+        }
     }
-    
-    //cell.textLabel.text = [objects objectAtIndex:indexPath.row].name;
-    return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 2;
+    
+    return form.sections[section].inputs.count;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return form.sections.count;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return @"INPUT";
+    return form.sections[section].sectionName;
 }
 
 @end
